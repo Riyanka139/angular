834349b78a0230e72eba7f6eed63680e7ea42d05
Riyanka <62233445+Riyanka139@users.noreply.github.com>
2023-06-28 12:08:50 +0530

    example of pipe to transform output

---
+++ src/app/filter.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter',
+  pure: false
+})
+export class FilterPipe implements PipeTransform {
+  transform(value: any, filterString: string, propName: string) {
+    if (value.length === 0 || filterString.length === 0) return value;
+
+    return value.filter((item) => item[propName] === filterString);
+  }
+}
